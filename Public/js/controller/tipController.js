4e287250eeb9f6c66240768972fa5b6263e52120
Arik <erikbrodch@gmail.com>
2016-11-30 14:11:57 +0200

    finished susection 2

--- Public/js/controller/tipController.js
+++ Public/js/controller/tipController.js
@@ -2,9 +2,25 @@ app.controller('tipController', ['$scope','tipService',function($scope, tipServi
 
 $scope.allStocks = tipService.allStocks;
 
+$scope.singleStock = tipService.singleStock;
+
 $scope.getStocks = function() {
 
     tipService.getStocks();
 }
 
+$scope.alertUid = function(index){
+    alert (tipService.allStocks[index].uid);
+}
+
+$scope.alertIndex = function(index){
+    alert (index);
+}
+
+$scope.submitLabel= function(){
+    var newLabel = {
+        stock_label: $scope.stock_label
+    } 
+    tipService.submitLabel(newLabel); 
+}
 }]);
--- Public/js/service/tipService.js
+++ Public/js/service/tipService.js
@@ -2,29 +2,39 @@ app.service('tipService', function () {
 
     var tipService = {
         allStocks: [],
+        singleStock: [],
 
         getStocks: function () {
-                $.ajax({
-                    method: "GET",
-                    url: 'http://trautocomplete.azurewebsites.net/api/Autocomplete/GetAutocomplete?name=M',
-                    dataType: "json",
-                    success: function (data) {
-                        for (var i = 0; i < 8; i++) {
-                            tipService.allStocks.push(data[i]);
-                        }
-                        // var stock = {
-                        //     ticker: data.ticker,
-                        //     label: data.label,
-                        //     uid: data.uid,
-                        //     value: data.value
-                        // }
-                        
-
-                    },
-                    error: function () {
-                        console.log("error");
+            $.ajax({
+                method: "GET",
+                url: 'http://trautocomplete.azurewebsites.net/api/Autocomplete/GetAutocomplete?name=M',
+                dataType: "json",
+                success: function (data) {
+                    console.log(data)
+                    for (var i = 0; i < 8; i++) {
+                        tipService.allStocks.push(data[i]);
                     }
-                });
+                },
+                error: function () {
+                    console.log("error");
+                }
+            });
+        },
+
+        submitLabel: function (newLabel) {
+            $.ajax({
+                method: "GET",
+                url: 'http://trautocomplete.azurewebsites.net/api/Autocomplete/GetAutocomplete?name='+ newLabel.stock_label,
+                dataType: "json",
+                success: function (data) {
+                    console.log(data);
+                        tipService.singleStock.push(data[0]);
+                },
+                error: function () {
+                    console.log("error");
+                }
+            });
+
         }
     }
 
